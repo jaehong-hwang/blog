9a64a56b094096dbff3f554572effcf6dc9b404a
Jaehong Hwang <krusnocide@gmail.com>
2019-07-26 22:13:52 +0900

    route, lint update

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,4 @@
-import Home from '@/views/Home.vue';
+import Home from '@/views/Home'
 
 export default [
   {
@@ -7,11 +7,8 @@ export default [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
+    path: '/portfolio',
+    name: 'portfolio',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/Portfolio'),
   },
-];
+]
